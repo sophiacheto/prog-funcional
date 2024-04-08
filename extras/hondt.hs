341c4f9e49f45b9fdef1483e88e503cd91c4874d
Sophia Cheto <sophiacheto@gmail.com>
2024-04-08 19:50:26 +0100

    update

--- extras/hondt.hs
+++ extras/hondt.hs
@@ -3,8 +3,12 @@ maxIndex xs = maximum (indices xs)
     where indices xs = zip xs [a | a <- [0, 1..]]
 
 
-hondt :: Int -> [Int] -> [Int]
-hondt num votos = rodar num votos [0 | x<-votos]
+-- atribuir mais um mandato a um partido
+atribuir :: [Int] -> [Int] -> [Int]
+atribuir votos eleitos = take indice eleitos ++ [valor + 1] ++ drop (indice + 1) eleitos
+        where quociente = [x `div` (1+y)| (x,y) <- zip votos eleitos]
+              indice = snd (maxIndex quociente)
+              valor = eleitos !! indice
 
 
 rodar :: Int -> [Int] -> [Int] -> [Int]
@@ -13,10 +17,5 @@ rodar n votos eleitos
         | otherwise = rodar (n-1) votos (atribuir votos eleitos)
 
 
--- atribuir mais um mandato a um partido
-atribuir :: [Int] -> [Int] -> [Int]
-atribuir votos eleitos = take indice eleitos ++ [valor + 1] ++ drop (indice + 1) eleitos
-        where quociente = [x `div` (1+y)| (x,y) <- zip votos eleitos]
-              indice = snd (maxIndex quociente)
-              valor = eleitos !! indice
-
+hondt :: Int -> [Int] -> [Int]
+hondt num votos = rodar num votos [0 | x<-votos]
